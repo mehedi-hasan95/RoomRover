ce987066f7da63f12f249cab778979638ae1d846
mehedi-hasan95 <mh.me.mehedi@gmail.com>
2024-02-03 20:44:15 +0600

    Initial setup done with authintication functionality

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["res.cloudinary.com", "avatars.githubusercontent.com"],
+  },
+};
+
+export default nextConfig;
---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+import { Header } from "@/components/auth/header";
+import { LoginButton } from "@/components/auth/login-button";
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main className="commonCss">
+      <div className="space-y-6 text-center flex flex-col">
+        <Header title="Wellcome to my demo" label="What are you want for?" />
+        <LoginButton mode="modal" asChild>
+          <Button>Login</Button>
+        </LoginButton>
+      </div>
+    </main>
+  );
+}
